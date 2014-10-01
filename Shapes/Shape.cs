15d89a28bb12ad7abca160e637ada349ce50588c
proalex <proallex@icloud.com>
2014-10-01 19:38:55 +0300

    Added: lab3.

---
+++ Shapes/Shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+namespace Shapes
+{
+    [Serializable()]
+    public abstract class Shape
+    {
+        public abstract void Draw(Graphics graphics, Pen pen);
+        public abstract void Update(Point start, Point end);
+    }
+}
---
+++ Shapes/Square.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shapes
+{
+    [Serializable()]
+    public class Square : Rectangle
+    {
+        public Square(int x, int y, int size) : base(x, y, size, size)
+        {
+
+        }
+
+        public Square(Point start, Point end)
+            : base(start.X, start.Y, end.Y - start.Y, end.Y - start.Y)
+        {
+
+        }
+
+        public override void Update(Point start, Point end)
+        {
+            end.X = start.X + (end.Y - start.Y);
+            base.Update(start, end);
+        }
+    }
+}
